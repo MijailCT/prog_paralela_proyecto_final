2875925c0a89141364da1a5bb82a4bab2734492e
MijailCT <MijailCT@users.noreply.github.com>
2025-12-02 22:28:12 -0400

    Adicion de base primitiva Co-authored-by-mijailCortesTaveras

---
+++ src/Program.cs
@@ -0,0 +1,127 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ProyectoFinalProgramacionParalela
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Logs debugLogs = new Logs(LogsNivel.DEBUG);
+            //NOTA: esto es una base primitiva adonde empezar a trabajar, 
+            // a medida que avancemos esto se hara haciendo mejor.
+
+            Console.WriteLine("Buscador de texto en archivos V0.1");
+
+            //TODO: hacer un loop con todo esto
+            //TODO: hacer todo esto de forma asincrona(?)
+            Console.Write("Directorio de trabajo: ");
+            string dir = Console.ReadLine() ?? "./";
+            if (!Directory.Exists(dir))
+            {
+                Console.WriteLine("ERROR: El directorio de trabajo proporcionado no existe.");
+                return;
+            }
+
+            MotorBusquedaSingleton motorBusqueda = new MotorBusquedaSingleton();
+            MotorSugerenciasSingleton motorSugerencias = new MotorSugerenciasSingleton();
+            MetricasSingleton metricas = new MetricasSingleton();
+            DatosSingleton capaDatos = new DatosSingleton();
+            //List<File> files = Directory.GetFiles(dir); (un suponer)
+            Console.WriteLine("Escribe tu busqueda, al iniciar se le recomendara " +
+            "palabras que podria utilizar, estas pueden aceptarse con la tecla TAB.");
+            Console.WriteLine("Para buscar el texto en los archivos tienes que presionar la tecla ENTER.");
+            Console.WriteLine("NOTA: Al darle a la tecla enter, se perdera la recomendacion.");
+            Console.Write("Busqueda: ");
+
+            string input = "";
+            ConsoleKeyInfo key;
+            while ((key = Console.ReadKey(true)).Key != ConsoleKey.Enter)
+            {
+                if (key.Key == ConsoleKey.Backspace)
+                {
+                    if (input.Length == 0) continue;
+                    input = input.Substring(0, input.Length - 1);
+                    var (left, top) = Console.GetCursorPosition();
+                    Console.SetCursorPosition(left - 1, top);
+                    Console.Write(' ');
+                    Console.SetCursorPosition(left - 1, top);
+                    Console.Out.Flush(); //por si acaso
+                }
+                else
+                {
+                    input += key.KeyChar;
+                    Console.Write(key.KeyChar);
+                }
+                //motorSugerencias.Predecir(input); (un suponer)
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Buscando el texto {input} en archivos...");
+            //motorBusqueda.Buscar(dir, input); (un suponer)
+        }
+    }
+    //TODO: mover cada clase a archivos independientes para mejor organizacion
+    public class MotorBusquedaSingleton
+    {
+
+    }
+
+    public class MotorSugerenciasSingleton
+    {
+
+    }
+
+    public class MetricasSingleton
+    {
+
+    }
+
+    //OJO: Capa de datos
+    public class DatosSingleton
+    {
+
+    }
+
+    public class ConfiguracionSingleton
+    {
+
+    }
+
+    public enum LogsNivel
+    {
+        DEBUG,
+        INFO,
+        WARN,
+        ERROR
+    }
+    
+    public class Logs
+    {
+        private string logFilePath;
+        private LogsNivel nivel_minimo;
+
+        public Logs(LogsNivel nivel = LogsNivel.INFO)
+        {
+            logFilePath = $"log-{DateTime.Now.ToString("O")}.txt";
+            nivel_minimo = nivel;
+        }
+
+        public Logs(string path, bool useDate = true, LogsNivel nivel = LogsNivel.INFO)
+        {
+            if (useDate) logFilePath = $"{path}-{DateTime.Now.ToString("O")}.txt";
+            else logFilePath = path;
+            nivel_minimo = nivel;
+        }
+
+        public void Write(string txt, LogsNivel? nivel)
+        {
+            var nvl = nivel ?? nivel_minimo;
+            if(nvl >= nivel_minimo) File.AppendAllText(logFilePath, $"[{nvl.ToString()}] {txt}");
+        }
+
+        public void WriteLine(string txt, LogsNivel? nivel)
+        {
+            var nvl = nivel ?? nivel_minimo;
+            if((nivel ?? nivel_minimo) >= nivel_minimo) File.AppendAllText(logFilePath, $"[{nvl.ToString()}] {txt}\n");
+        }
+    }
+};
