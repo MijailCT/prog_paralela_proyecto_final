81eb7e778c3a32ea5f42bd689a2a9bb1a0f0a068
MijailCT <MijailCT@users.noreply.github.com>
2025-12-03 22:17:02 -0400

    Implementacion del motor de busqueda inicial Co-authored-by-mijailCortesTaveras

--- src/MotorBusqueda.cs
+++ src/MotorBusqueda.cs
@@ -1,3 +1,5 @@
+using System.Collections.Concurrent;
+
 namespace ProyectoFinalProgramacionParalela
 {
     public class MotorBusquedaSingleton
@@ -5,7 +7,35 @@ namespace ProyectoFinalProgramacionParalela
         private static MotorBusquedaSingleton instance = null;
         private static readonly object _lock = new object();
 
-        MotorBusquedaSingleton() { }
+        private string directorioTrabajo;
+        private ParallelOptions opcionesParalelas;
+
+        MotorBusquedaSingleton(string directorio, ParallelOptions opt)
+        {
+            directorioTrabajo = directorio;
+            opcionesParalelas = opt;
+        }
+        
+        public async Task<List<string>> Buscar(string texto)
+        {
+            //Enumeramos toooodos los archivos (incluyendo los que estan adentro de los directorios) de nuestro directorio de trabajo
+            //TODO: reemplazarlo con una funcion que enumere los archivos y despues los organize segun los puntajes
+            var archivos = Directory.EnumerateFiles(directorioTrabajo, "*.txt", SearchOption.AllDirectories);
+            //Las concurrentbags sirven para recolectar datos de una forma thread-safe (osea, sin condiciones de carrera)
+            var resultadosBag = new ConcurrentBag<string>();
+            Parallel.ForEach(archivos, opcionesParalelas,
+            (archivoPath) =>
+            {
+                string textoArchivo = File.ReadAllText(archivoPath);
+                if (textoArchivo.Contains(texto, StringComparison.OrdinalIgnoreCase))
+                {
+                    resultadosBag.Add(archivoPath);
+                    //DatosSingleton.Datos.IncrementarPuntaje(archivoPath); (un suponer)
+                }
+            });
+            
+            return resultadosBag.ToList();
+        }
         
         public static MotorBusquedaSingleton MotorBusqueda
         {
@@ -15,7 +45,8 @@ namespace ProyectoFinalProgramacionParalela
                 {
                     if (instance == null)
                     {
-                        instance = new MotorBusquedaSingleton();
+                        var conf = ConfiguracionSingleton.Configuracion;
+                        instance = new MotorBusquedaSingleton(conf.GetDirectorio(), conf.GetOpcionesParalelas());
                     }
                     return instance;
                 }
