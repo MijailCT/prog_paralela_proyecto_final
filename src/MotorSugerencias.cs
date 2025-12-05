755615455537ef38bbe9b23888bb4b534ba5ddc5
danielthomas.xd99@outlook.com <danielthomas.xd99@outlook.com>
2025-12-04 21:04:00 -0400

    feat(motor-especulacion): inicializar singleton y estructura base Co-authored-byDanielLeonDeThomas

--- src/MotorSugerencias.cs
+++ src/MotorSugerencias.cs
@@ -1,3 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace ProyectoFinalProgramacionParalela
 {
     public class MotorSugerenciasSingleton
@@ -5,8 +12,19 @@ namespace ProyectoFinalProgramacionParalela
         private static MotorSugerenciasSingleton instance = null;
         private static readonly object _lock = new object();
 
-        MotorSugerenciasSingleton() { }
-        
+        private MotorSugerenciasSingleton()
+        {
+            palabrasComunes = new List<string>
+            {
+                "algoritmo", "paralelo", "hilo", "tarea", "buscar", "archivo", "texto",
+                "especulativo", "predicción", "sugerencia", "compleción", "código",
+                "programación", "multihilo", "procesador", "núcleo", "rendimiento"
+            };
+
+            bigramas = new Dictionary<string, List<string>>(StringComparer.OrdinalIgnoreCase);
+            ConstruirBigramas();
+        }
+
         public static MotorSugerenciasSingleton MotorSugerencias
         {
             get
@@ -14,9 +32,7 @@ namespace ProyectoFinalProgramacionParalela
                 lock (_lock)
                 {
                     if (instance == null)
-                    {
                         instance = new MotorSugerenciasSingleton();
-                    }
                     return instance;
                 }
             }
